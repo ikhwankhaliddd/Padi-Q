30a037144b3d2db1b2f81bd9525a09dfba02fd99
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2022-03-12 00:11:31 +0700

    Modify app.py to fix deployment

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ async def predict(file: UploadFile = File(...)):
     }
 
 if __name__ == "__app__" :
-     uvicorn.run(app, host = '127.0.0.1', port = 8000, debug = True)
+     uvicorn.run(app, host = '0.0.0.0', port = 5000, debug = True)
